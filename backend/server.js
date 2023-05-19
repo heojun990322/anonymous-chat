78f160b0d474d0c71142f09c23907b67f7e86b80
Jun Heo <heojun990322@gmail.com>
2023-05-19 16:08:54 +0900

    Feat: create Express JS API

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("API is running..");
-})
+    res.send("API is running successfully!");
+});
 
-app.listen(8000, console.log("Server running on PORT 8000"));
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
