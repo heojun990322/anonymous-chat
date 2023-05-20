b095f0b018efdff631098c888463183c77e832d8
Jun Heo <heojun990322@gmail.com>
2023-05-21 03:40:13 +0900

    Feat(backend): add errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 
@@ -18,6 +19,10 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+
+// error 처리
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, console.log(`Server running on PORT ${PORT}`.yellow.bold));
