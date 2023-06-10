0407d3e0284e042099992ce57984d7b0f96506b2
Jun Heo <heojun990322@gmail.com>
2023-06-10 20:39:12 +0900

    Feat(frontend): axios uses baseURL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import theme from './theme';
 import ChatProvider from './context/ChatProvider';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
