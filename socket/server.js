6216d32d791168c8879cf65f15ffb050bcdc1973
Jun Heo <heojun990322@gmail.com>
2023-06-11 17:48:28 +0900

    Refactor: seperate socket from api

--- socket/server.js
+++ socket/server.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-const colors = require("colors");
-const userRoutes = require("./routes/userRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-const messageRoutes = require("./routes/messageRoutes");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors");
+const colors = require("colors");
 
 dotenv.config();
 
-connectDB();
 const app = express();
 
 // JSON 데이터를 받기 위함
@@ -26,24 +20,15 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.send("API is running successfully!");
+  res.send("Socket server is running successfully!");
 });
 
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
-app.use("/api/message", messageRoutes);
-
-// error 처리
-app.use(notFound);
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(`Server running on PORT ${PORT}`.yellow.bold)
 );
 
-/*
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
@@ -97,4 +82,3 @@ io.on("connection", (socket) => {
     });
   });
 });
-*/
